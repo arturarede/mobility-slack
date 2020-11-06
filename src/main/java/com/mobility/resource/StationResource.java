cc24a34ca290477d0cb3ef275a786d54160e9ee7
Artur Arede <arturricardo_a@hotmail.com>
2020-11-06 22:47:13 +0000

    Station Tests

--- src/main/java/com/mobility/resource/StationResource.java
+++ src/main/java/com/mobility/resource/StationResource.java
@@ -1,23 +1,15 @@
 package com.mobility.resource;
 
+import com.mobility.model.entity.StationType;
 import com.mobility.resource.dto.StationDto;
-import com.mobility.mapper.StationMapper;
-import com.mobility.model.entity.Station;
 import com.mobility.service.StationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.Map;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/stations")
@@ -26,42 +18,21 @@ public class StationResource {
     @Autowired
     StationService stationService;
 
-    @Autowired
-    StationMapper stationMapper;
-
     @GetMapping(path = "")
     public ResponseEntity<Map<String, Object>> listStations(@RequestParam(required = false) String name,
-                                                            @RequestParam(required = false) String type,
+                                                            @RequestParam(required = false) StationType type,
                                                             @RequestParam(defaultValue = "0") int page,
                                                             @RequestParam(defaultValue = "10") int size) {
-        // @TODO fix this code
-        if(name != null) {
-            Station station = stationService.getStationByName(name);
-            Map<String, Object> response = new HashMap<>();
-            response.put("station", station);
-            return new ResponseEntity<>(response, HttpStatus.OK);
-        }
 
-        Pageable paging = PageRequest.of(page, size);
-        Page<Station> stations = stationService.listStations(paging);
-        Map<String, Object> response = new HashMap<>();
-        response.put("stations", stations.get());
-        response.put("currentPage", stations.getNumber());
-        response.put("totalItems", stations.getTotalElements());
-        response.put("totalPages", stations.getTotalPages());
-        // @TODO return CollectionModel
+        Map<String, Object> response = stationService.listStations(name, type, page, size);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @GetMapping(path = "/{id}")
     public EntityModel<StationDto> getStationById(@PathVariable("id") Integer id) {
-        Station station = stationService.getStationById(id);
-        StationDto stationDto = stationMapper.stationToStationDTO(station);
-        EntityModel<StationDto> stationDtoEntityModel = EntityModel.of(stationDto);
-        stationDtoEntityModel.add(linkTo(StationResource.class).slash(station.getId()).withSelfRel());
-        stationDtoEntityModel.add(linkTo(methodOn(StationResource.class).listStations(null, null,
-                0, 10)).withRel("stations"));
-        return stationDtoEntityModel;
+        return stationService.getStationById(id);
     }
+
+
 
 }
