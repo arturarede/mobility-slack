e7acd3772bbb28b3031a2f829ade3ac58ad04a96
Artur Arede <arturricardo_a@hotmail.com>
2020-11-07 16:46:27 +0000

    Station Resource Tests

--- src/main/java/com/mobility/resource/dto/StationDto.java
+++ src/main/java/com/mobility/resource/dto/StationDto.java
@@ -3,10 +3,12 @@ package com.mobility.resource.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.hateoas.server.core.Relation;
 
 @Data
+@EqualsAndHashCode(callSuper=false)
 @Relation(collectionRelation = "stations", itemRelation = "station")
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
--- src/test/java/com/mobility/resource/StationResourceTest.java
+++ src/test/java/com/mobility/resource/StationResourceTest.java
@@ -8,12 +8,19 @@ import com.mobility.service.exceptions.MobilityNotFoundException;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.restdocs.payload.JsonFieldType;
 
+import java.util.List;
 import java.util.Optional;
 
 import static io.restassured.RestAssured.given;
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 
 public class StationResourceTest extends AbstractTestConfiguration {
@@ -41,6 +48,9 @@ public class StationResourceTest extends AbstractTestConfiguration {
 								fieldWithPath("stationType").type(JsonFieldType.STRING).description("The Station type, it can be \"CP (Comboios de Portugal)\" or \"Metro\"."),
 								subsectionWithPath("_links").type(JsonFieldType.OBJECT).description("HATEOAS Links " +
 										"in HAL Format. Delivering every possible next action a client may perform.")
+						),
+						links(
+								linkWithRel("self").description("Reference to self.")
 						)
 				))
 				.when()
@@ -99,31 +109,44 @@ public class StationResourceTest extends AbstractTestConfiguration {
 	@Test
 	public void whenListAllStations_thenReturnPaginatedStations() {
 		// given
-		Station station = StationBuilder
-				.aMetroStation()
+		int pageNr = 0;
+		int size = 10;
+		Pageable pageable = PageRequest.of(pageNr, size);
+
+		Station stationCp = StationBuilder
+				.aCpStation().but()
+				.withId(1)
 				.build();
 
-		when(stationRepository.findById(station.getId())).thenReturn(Optional.of(station));
+		Station stationMetro = StationBuilder
+				.aMetroStation().but()
+				.withId(2)
+				.build();
+
+		Page<Station> page = new PageImpl<>(List.of(stationCp, stationMetro));
+		when(stationRepository.findAll(pageable)).thenReturn(page);
 
 		// when
 		Response response = given(documentationSpec)
-				.filter(document(RESOURCE + "whenCallingStationByIdEndpoint_thenReturnStation",
+				.filter(document(RESOURCE + "whenListAllStations_thenReturnPaginatedStations",
 						responseFields(
-								fieldWithPath("stationId").type(JsonFieldType.NUMBER).description("The Station id."),
-								fieldWithPath("stationName").type(JsonFieldType.STRING).description("The Station name."),
-								fieldWithPath("stationType").type(JsonFieldType.STRING).description("The Station type, it can be \"CP (Comboios de Portugal)\" or \"Metro\"."),
-								subsectionWithPath("_links").type(JsonFieldType.OBJECT).description("HATEOAS Links " +
-										"in HAL Format. Delivering every possible next action a client may perform.")
+								fieldWithPath("_embedded.stations[].stationId").type(JsonFieldType.NUMBER).description("The Station id."),
+								fieldWithPath("_embedded.stations[].stationName").type(JsonFieldType.STRING).description("The Station name."),
+								fieldWithPath("_embedded.stations[].stationType").type(JsonFieldType.STRING).description("The Station type, it can be \"CP (Comboios de Portugal)\" or \"Metro\"."),
+								subsectionWithPath("_embedded.stations[]._links").type(JsonFieldType.OBJECT).description("Self Link to a station"),
+								subsectionWithPath("._links").type(JsonFieldType.OBJECT).description("Link to list stations"),
+								subsectionWithPath("page").type(JsonFieldType.OBJECT).description("Paging information")
 						)
 				))
 				.when()
 				.port(port)
-				.pathParam("id", station.getId())
-				.get("/stations/{id}");
+				.get("/stations/");
 
 
 		// then
 		response.then().statusCode(200);
 	}
+
+
 
 }
