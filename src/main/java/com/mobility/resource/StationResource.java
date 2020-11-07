0379a18da9ce9b0a2b338629f13393f648e7d1da
Artur Arede <arturricardo_a@hotmail.com>
2020-11-07 08:11:49 +0000

    Station DTO refactor

--- src/main/java/com/mobility/resource/StationResource.java
+++ src/main/java/com/mobility/resource/StationResource.java
@@ -1,15 +1,26 @@
 package com.mobility.resource;
 
+import com.mobility.mapper.StationMapper;
+import com.mobility.model.entity.Station;
 import com.mobility.model.entity.StationType;
 import com.mobility.resource.dto.StationDto;
 import com.mobility.service.StationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/stations")
@@ -18,21 +29,47 @@ public class StationResource {
     @Autowired
     StationService stationService;
 
+    @Autowired
+    StationMapper stationMapper;
+
     @GetMapping(path = "")
     public ResponseEntity<Map<String, Object>> listStations(@RequestParam(required = false) String name,
                                                             @RequestParam(required = false) StationType type,
                                                             @RequestParam(defaultValue = "0") int page,
                                                             @RequestParam(defaultValue = "10") int size) {
 
-        Map<String, Object> response = stationService.listStations(name, type, page, size);
+        Map<String, Object> response = new LinkedHashMap<>();
+        if(name != null) {
+            Station station = stationService.getStationByName(name);
+            EntityModel<StationDto> stationDto = createStationDtoEntityModel(station);
+            response.put("stations", stationDto);
+        } else {
+            Pageable paging = PageRequest.of(page, size);
+            Page<Station> stations = stationService.listStations(paging);
+            List<EntityModel<StationDto>> stationsDto = stations.getContent().stream()
+                    .map(this::createStationDtoEntityModel)
+                    .collect(Collectors.toList());
+            response.put("totalItems", stations.getTotalElements());
+            response.put("stations", stationsDto);
+            response.put("totalPages", stations.getTotalPages());
+            response.put("currentPage", stations.getNumber());
+        }
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @GetMapping(path = "/{id}")
     public EntityModel<StationDto> getStationById(@PathVariable("id") Integer id) {
-        return stationService.getStationById(id);
+        Station station = stationService.getStationById(id);
+        EntityModel<StationDto> stationDtoEntityModel = createStationDtoEntityModel(station);
+        stationDtoEntityModel.add(linkTo(methodOn(StationResource.class).listStations(null, null,
+                0, 10)).withRel("stations"));
+        return stationDtoEntityModel;
     }
 
-
-
+    private EntityModel<StationDto> createStationDtoEntityModel(Station station) {
+        StationDto stationDto = stationMapper.stationToStationDTO(station);
+        EntityModel<StationDto> stationDtoEntityModel = EntityModel.of(stationDto);
+        stationDtoEntityModel.add(linkTo(StationResource.class).slash(station.getId()).withSelfRel());
+        return stationDtoEntityModel;
+    }
 }
--- src/main/java/com/mobility/service/StationService.java
+++ src/main/java/com/mobility/service/StationService.java
@@ -1,75 +1,32 @@
 package com.mobility.service;
 
-import com.mobility.mapper.StationMapper;
 import com.mobility.model.entity.Station;
-import com.mobility.model.entity.StationType;
 import com.mobility.repository.StationRepository;
-import com.mobility.resource.StationResource;
-import com.mobility.resource.dto.StationDto;
 import com.mobility.service.exceptions.MobilityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.hateoas.EntityModel;
 import org.springframework.stereotype.Service;
-
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Service
 public class StationService {
 
     @Autowired
-    StationMapper stationMapper;
-
-    @Autowired
     StationRepository stationRepository;
 
-    public Map<String, Object> listStations(String name, StationType type, int page, int size) {
-        Map<String, Object> response = new LinkedHashMap<>();
-        if(name != null) {
-            Station station = this.getStationByName(name);
-            EntityModel<StationDto> stationDto = createStationDtoEntityModel(station);
-            response.put("stations", stationDto);
-        } else {
-            Pageable paging = PageRequest.of(page, size);
-            Page<Station> stations = stationRepository.findAll(paging);
-            List<EntityModel<StationDto>> stationsDto = stations.getContent().stream()
-                    .map(this::createStationDtoEntityModel)
-                    .collect(Collectors.toList());
-            response.put("totalItems", stations.getTotalElements());
-            response.put("stations", stationsDto);
-            response.put("totalPages", stations.getTotalPages());
-            response.put("currentPage", stations.getNumber());
-        }
-        return response;
+    public Page<Station> listStations(Pageable page) {
+        return stationRepository.findAll(page);
     }
 
-    public EntityModel<StationDto> getStationById(Integer id) {
-        Station station = stationRepository.findById(id)
+    public Station getStationById(Integer id) {
+        return stationRepository.findById(id)
                 .orElseThrow(() -> new MobilityNotFoundException("Station with id: " + id + " does not exist"));
-        EntityModel<StationDto> stationDtoEntityModel = createStationDtoEntityModel(station);
-        stationDtoEntityModel.add(linkTo(methodOn(StationResource.class).listStations(null, null,
-                0, 10)).withRel("stations"));
-        return stationDtoEntityModel;
+
     }
 
-    private Station getStationByName(String name) {
+    public Station getStationByName(String name) {
         return stationRepository.findByNameIgnoreCase(name)
                 .orElseThrow(() -> new MobilityNotFoundException("Station with name " + name + " does not exist"));
-    }
-
-    private EntityModel<StationDto> createStationDtoEntityModel(Station station) {
-        StationDto stationDto = stationMapper.stationToStationDTO(station);
-        EntityModel<StationDto> stationDtoEntityModel = EntityModel.of(stationDto);
-        stationDtoEntityModel.add(linkTo(StationResource.class).slash(station.getId()).withSelfRel());
-        return stationDtoEntityModel;
     }
 
 }
