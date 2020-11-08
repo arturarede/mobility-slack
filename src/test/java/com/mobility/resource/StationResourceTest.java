0f3bbf0109557ebaedefbe65b70978dfdd55edec
Artur Arede <arturricardo_a@hotmail.com>
2020-11-08 23:47:14 +0000

    Train resource tests

--- src/test/java/com/mobility/resource/StationResourceTest.java
+++ src/test/java/com/mobility/resource/StationResourceTest.java
@@ -1,11 +1,11 @@
 package com.mobility.resource;
 
 import com.mobility.configuration.AbstractTestConfiguration;
+import com.mobility.configuration.exceptions.MobilityNotFoundException;
 import com.mobility.helpers.StationBuilder;
 import com.mobility.model.entity.Station;
 import com.mobility.model.entity.StationType;
 import com.mobility.repository.StationRepository;
-import com.mobility.configuration.exceptions.MobilityNotFoundException;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -32,80 +32,6 @@ public class StationResourceTest extends AbstractTestConfiguration {
 
 	@MockBean
 	private StationRepository stationRepository;
-
-	@Test
-	public void whenCallingStationById_thenReturnStation() {
-		// given
-		Station station = StationBuilder
-				.aMetroStation()
-				.build();
-
-		when(stationRepository.findById(station.getId())).thenReturn(Optional.of(station));
-
-		// when
-		Response response = given(documentationSpec)
-				.filter(document(RESOURCE + "whenCallingStationById_thenReturnStation",
-						responseFields(
-								fieldWithPath("stationId").type(JsonFieldType.NUMBER).description("The Station id."),
-								fieldWithPath("stationName").type(JsonFieldType.STRING).description("The Station name."),
-								fieldWithPath("stationType").type(JsonFieldType.STRING).description("The Station type, it can be \"CP (Comboios de Portugal)\" or \"Metro\"."),
-								fieldWithPath("_links.self.href").type(JsonFieldType.STRING).description("HATEOAS Links " +
-										"in HAL Format. Delivering every possible next action a client may perform.")
-						),
-						links(
-								linkWithRel("self").description("Reference to self.")
-						)
-				))
-				.when()
-				.port(port)
-				.pathParam("id", station.getId())
-				.get("/stations/{id}");
-
-
-		// then
-		response.then().statusCode(200);
-	}
-
-	@Test
-	public void givenStationIdDoesNotExist_whenCallingStationById_thenReturnNotFoundException() {
-		// given
-		Station station = StationBuilder
-				.aStation()
-				.build();
-
-		when(stationRepository.findById(station.getId())).thenThrow(MobilityNotFoundException.class);
-
-		// when
-		Response response = given(documentationSpec)
-				.filter(document(RESOURCE + "givenStationIdDoesNotExist_whenCallingStationById_thenReturnNotFoundException"))
-				.when()
-				.port(port)
-				.pathParam("id", station.getId())
-				.get("/stations/{id}");
-
-
-		// then
-		response.then()
-				.statusCode(404);
-	}
-
-	@Test
-	public void givenStationIdNotNumber_whenCallingStationById_thenReturnBadRequestException() {
-		// given
-		String id = "I'm a String";
-		// when
-		Response response = given(documentationSpec)
-				.filter(document(RESOURCE + "givenStationIdNotNumber_whenCallingStationById_thenReturnBadRequestException"))
-				.when()
-				.port(port)
-				.pathParam("id", id)
-				.get("/stations/{id}");
-
-
-		// then
-		response.then()
-				.statusCode(400);
-	}
 
 	@Test
 	public void whenListAllStations_thenReturnPaginatedStations() {
@@ -447,5 +373,78 @@ public class StationResourceTest extends AbstractTestConfiguration {
 		response.then().statusCode(200);
 	}
 
+	@Test
+	public void whenCallingStationById_thenReturnStation() {
+		// given
+		Station station = StationBuilder
+				.aMetroStation()
+				.build();
+
+		when(stationRepository.findById(station.getId())).thenReturn(Optional.of(station));
+
+		// when
+		Response response = given(documentationSpec)
+				.filter(document(RESOURCE + "whenCallingStationById_thenReturnStation",
+						responseFields(
+								fieldWithPath("stationId").type(JsonFieldType.NUMBER).description("The Station id."),
+								fieldWithPath("stationName").type(JsonFieldType.STRING).description("The Station name."),
+								fieldWithPath("stationType").type(JsonFieldType.STRING).description("The Station type, it can be \"CP (Comboios de Portugal)\" or \"Metro\"."),
+								fieldWithPath("_links.self.href").type(JsonFieldType.STRING).description("HATEOAS Links " +
+										"in HAL Format. Delivering every possible next action a client may perform.")
+						),
+						links(
+								linkWithRel("self").description("Reference to self.")
+						)
+				))
+				.when()
+				.port(port)
+				.pathParam("id", station.getId())
+				.get("/stations/{id}");
+
+
+		// then
+		response.then().statusCode(200);
+	}
+
+	@Test
+	public void givenStationIdDoesNotExist_whenCallingStationById_thenReturnNotFoundException() {
+		// given
+		Station station = StationBuilder
+				.aStation()
+				.build();
+
+		when(stationRepository.findById(station.getId())).thenThrow(MobilityNotFoundException.class);
+
+		// when
+		Response response = given(documentationSpec)
+				.filter(document(RESOURCE + "givenStationIdDoesNotExist_whenCallingStationById_thenReturnNotFoundException"))
+				.when()
+				.port(port)
+				.pathParam("id", station.getId())
+				.get("/stations/{id}");
+
+
+		// then
+		response.then()
+				.statusCode(404);
+	}
+
+	@Test
+	public void givenStationIdNotNumber_whenCallingStationById_thenReturnBadRequestException() {
+		// given
+		String id = "I'm a String";
+		// when
+		Response response = given(documentationSpec)
+				.filter(document(RESOURCE + "givenStationIdNotNumber_whenCallingStationById_thenReturnBadRequestException"))
+				.when()
+				.port(port)
+				.pathParam("id", id)
+				.get("/stations/{id}");
+
+
+		// then
+		response.then()
+				.statusCode(400);
+	}
 
 }
--- src/test/java/com/mobility/resource/TrainResourceTest.java
+++ src/test/java/com/mobility/resource/TrainResourceTest.java
@@ -1,5 +1,6 @@
 package com.mobility.resource;
 
+import com.github.tomakehurst.wiremock.client.WireMock;
 import com.mobility.configuration.AbstractTestConfiguration;
 import com.mobility.helpers.StationBuilder;
 import com.mobility.model.entity.Station;
@@ -8,41 +9,63 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.restdocs.payload.JsonFieldType;
 
 import java.util.Optional;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static io.restassured.RestAssured.given;
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 
 public class TrainResourceTest extends AbstractTestConfiguration {
 
     private final String RESOURCE = "trains/";
-    private Station station;
 
     @MockBean
     private StationRepository stationRepository;
 
     @BeforeEach
     private void init() {
+        WireMock.reset();
 
-        station = StationBuilder
-                .aCpStation()
-                .build();
-
-        when(stationRepository.findById(station.getId())).thenReturn(Optional.of(station));
+        stubFor(get(anyUrl())
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json;charset=iso-8859-1")
+                        .withStatus(200)
+                        .withBodyFile("ip_response.json")));
     }
 
     @Test
     public void firstTestHere() {
-        stubFor(get(anyUrl())
-                .willReturn(aResponse().withHeader("Content-Type", "application/json;charset=iso-8859-1")
-                .withStatus(200)
-                .withBodyFile("ip_response.json")));
+        // given
+        Station station = StationBuilder
+                .aCpStation()
+                .build();
+
+        when(stationRepository.findById(station.getId())).thenReturn(Optional.of(station));
 
         // when
         Response response = given(documentationSpec)
+                .filter(document(RESOURCE + "firstTestHere",
+                        requestParameters(
+                                parameterWithName("state").optional().description("If it is from DEPARTURES or ARRIVALS"),
+                                parameterWithName("date").optional().description("Date from when to start the search (default = now())"),
+                                parameterWithName("withDelay").optional().description("Only look for the trains with delay")
+                        ),
+                        responseFields(
+                                fieldWithPath("_embedded.trains[].id").type(JsonFieldType.NUMBER).description("The Train id."),
+                                fieldWithPath("_embedded.trains[].trainType").type(JsonFieldType.STRING).description("The Train type."),
+                                fieldWithPath("_embedded.trains[].departure").type(JsonFieldType.STRING).description(""),
+                                fieldWithPath("_embedded.trains[].arrival").type(JsonFieldType.STRING).description(""),
+                                fieldWithPath("_embedded.trains[].delay").type(JsonFieldType.STRING).description(""),
+                                subsectionWithPath("_embedded.trains[]._links").type(JsonFieldType.OBJECT).description(""),
+                                subsectionWithPath("_links").type(JsonFieldType.OBJECT).description("HATEOAS Links " +
+                                        "in HAL Format. Delivering every possible next action a client may perform.")
+                        )
+                ))
                 .when()
                 .port(port)
                 .pathParam("station-id", station.getId())
